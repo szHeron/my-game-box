5b07f4815fa7ff710194df3ce583c7ae0c3b24ba
szheron <heronbr7@gmail.com>
2024-01-31 21:58:51 -0300

    feat: added main card

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,12 @@ import { BasicButton } from "./BasicButton";
 
 export function Header(){
     return (
-        <div className="flex flex-row items-center justify-around h-[10%] bg-secundaryBlue md:justify-between md:px-32">
-            <h1 className="text-3xl font-bold">
-                Gamebox
-            </h1>
+        <div className="flex flex-row items-center justify-around h-[8%] bg-secundaryBlue md:justify-between md:px-32">
+            <Link href="/">
+                <h1 className="text-3xl font-bold">
+                    Gamebox
+                </h1>
+            </Link>
             <Link href="/signin">
                 <BasicButton>
                     Login
